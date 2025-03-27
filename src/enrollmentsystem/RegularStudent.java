e561a5eb68bd746213e37e59f5ab9cbb5998b91f
ACER <ACER@DESKTOP-16AEI05>
2025-03-27 15:40:49 +0800

    SUNOD NA COMMIT

--- src/enrollmentsystem/RegularStudent.java
+++ src/enrollmentsystem/RegularStudent.java
@@ -9,12 +9,12 @@ package enrollmentsystem;
  *
  * @author ACER
  */
-public class StudentDashboard extends javax.swing.JFrame {
+public class RegularStudent extends javax.swing.JFrame {
 
     /**
      * Creates new form StudentDashboard
      */
-    public StudentDashboard() {
+    public RegularStudent() {
         initComponents();
     }
 
@@ -40,6 +40,7 @@ public class StudentDashboard extends javax.swing.JFrame {
         jButton8 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel4.setBackground(new java.awt.Color(255, 255, 255));
@@ -101,20 +102,21 @@ public class StudentDashboard extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(StudentDashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegularStudent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(StudentDashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegularStudent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(StudentDashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegularStudent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(StudentDashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegularStudent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new StudentDashboard().setVisible(true);
+                new RegularStudent().setVisible(true);
             }
         });
     }
